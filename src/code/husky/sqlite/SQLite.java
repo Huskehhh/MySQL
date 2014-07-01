b4c2768ebd4b4daab228cf66478b567f65f7f3ba
Aidan Matzko <amatzko48@gmail.com>
2014-07-01 17:06:53 -0400

    Add Folder creation to SQLite

--- src/code/husky/sqlite/SQLite.java
+++ src/code/husky/sqlite/SQLite.java
@@ -40,6 +40,9 @@ public class SQLite extends Database {
 	@Override
 	public Connection openConnection() throws SQLException,
 			ClassNotFoundException {
+		if (!plugin.getDataFolder().exists()) {
+			plugin.getDataFolder().mkdirs();
+		}
 		File file = new File(plugin.getDataFolder(), dbLocation);
 		if (!(file.exists())) {
 			try {
