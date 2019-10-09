72c0352724007fb6285e4a4930bfcb40cd7628bb
Jordyn Newnham <jordyn@husk.pro>
2019-10-09 21:19:33 +1100

    Update resource to use HikariCP

--- src/main/java/pro/husk/mysql/MySQL.java
+++ src/main/java/pro/husk/mysql/MySQL.java
@@ -1,94 +1,79 @@
 package pro.husk.mysql;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import pro.husk.Database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Connects to and uses a MySQL database
+ * Connects to and uses a MySQL database through Hikari connection pool
  *
  * @author Huskehhh
  * @author tips48
  */
 public class MySQL extends Database {
-    private final String user;
-    private final String database;
-    private final String password;
-    private final String port;
-    private final String hostname;
-    private final boolean useSSL;
-    private final boolean verifyServerCertificate;
 
-    /**
-     * Creates a new MySQL instance
-     *
-     * @param hostname                Name of the host
-     * @param port                    Port number
-     * @param username                Username
-     * @param password                Password
-     * @param useSSL                  SSL Property
-     * @param verifyServerCertificate SSL Property
-     */
-    public MySQL(String hostname, String port, String username,
-                 String password, boolean useSSL, boolean verifyServerCertificate) {
-        this(hostname, port, null, username, password, useSSL, verifyServerCertificate);
-    }
+    // Hikari datasource
+    private final HikariDataSource dataSource;
 
     /**
      * Creates a new MySQL instance for a specific database
      *
-     * @param hostname                Name of the host
-     * @param port                    Port number
-     * @param database                Database name
-     * @param username                Username
-     * @param password                Password
-     * @param useSSL                  SSL Property
-     * @param verifyServerCertificate SSL Property
+     * @param hostname | Name of the host
+     * @param port     | Port number
+     * @param database | Database name
+     * @param username | Username
+     * @param password | Password
+     * @param useSSL   | Use SSL
      */
-    public MySQL(String hostname, String port, String database,
-                 String username, String password, boolean useSSL, boolean verifyServerCertificate) {
-        this.hostname = hostname;
-        this.port = port;
-        this.database = database;
-        this.user = username;
-        this.password = password;
-        this.useSSL = useSSL;
-        this.verifyServerCertificate = verifyServerCertificate;
+    public MySQL(String hostname, String port, String database, String username, String password, boolean useSSL) {
+
+        // Build URL of database
+        StringBuilder urlBuild = new StringBuilder();
+        urlBuild.append("jdbc:mysql://");
+        urlBuild.append(hostname);
+        urlBuild.append(":");
+        urlBuild.append(port);
+        urlBuild.append("/");
+        urlBuild.append(database);
+
+        if (!useSSL) {
+            urlBuild.append("?useSSL=false");
+        }
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(urlBuild.toString());
+        config.setUsername(username);
+        config.setPassword(password);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        dataSource = new HikariDataSource(config);
     }
 
+    /**
+     * Getter for the Hikari DataSource object
+     *
+     * @return Hikari DataSource object
+     */
+    public HikariDataSource getDataSource() {
+        return dataSource;
+    }
+
+    /**
+     * Gets connection from Hikari DataSource
+     *
+     * @return connection
+     * @throws SQLException if cannot get a connection
+     */
     @Override
-    public Connection openConnection() throws SQLException,
-            ClassNotFoundException {
-        if (checkConnection()) {
-            return connection;
+    public Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = dataSource.getConnection();
         }
-
-        String connectionURL = "jdbc:mysql://"
-                + this.hostname + ":" + this.port;
-
-        if (database != null) {
-            connectionURL = connectionURL + "/" + this.database;
-        }
-        
-        // Refer: https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-using-ssl.html
-
-        if (verifyServerCertificate) {
-            System.setProperty("javax.net.ssl.trustStore", "path_to_truststore_file");
-            System.setProperty("javax.net.ssl.trustStorePassword", "mypassword");
-        }
-
-        // Append useSSL to end of connectionURL to avoid errors firing off with newer MySQL
-        connectionURL = connectionURL + "?useSSL=" + this.useSSL;
-
-        Class.forName("com.mysql.jdbc.Driver");
-        connection = DriverManager.getConnection(connectionURL,
-                this.user, this.password);
-
-        // Allows implementation of batch statements
-        connection.setAutoCommit(false);
-
         return connection;
     }
 }
